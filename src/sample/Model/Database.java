82a344ed4f50ee8de90dad625274805a5779450f
Jatin Nanda <jatin.nanda1337@gmail.com>
2016-03-23 22:27:58 -0400

    Create Table statements added as a txt file and integrated in Database class...

--- src/sample/Model/Database.java
+++ src/sample/Model/Database.java
@@ -1,6 +1,11 @@
 package sample.Model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 /**
@@ -24,5 +29,27 @@ public class Database {
             System.out.println("Jatin probably doesn't have the server running right now");
         }
         return null;
+    }
+
+    //note this method should only be called ONCE - when the tables are
+    // INITIALLY created
+    public static void setUp() throws Exception {
+        try {
+            Connection con = getConnection();
+            Statement createTables = con.createStatement();
+            ArrayList<String> createStatements = new ArrayList<>();
+            //reading table statements from a txt file because I'm lazy...
+            Scanner scanner = new Scanner(new File("tables.txt"));
+            scanner.useDelimiter(";");
+            //reads queries from file, separated by a ';'
+            while(scanner.hasNext()) {
+                createStatements.add(scanner.next());
+            }
+            for (String table : createStatements) {
+                createTables.executeQuery(table);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
