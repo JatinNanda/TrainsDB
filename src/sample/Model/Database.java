ccef411b1ba437ec5b15b4e97f413256855b09b9
Jatin Nanda <jatin.nanda1337@gmail.com>
2016-04-10 14:32:44 -0400

    Tables created; we're ready to roll

--- src/sample/Model/Database.java
+++ src/sample/Model/Database.java
@@ -1,8 +1,7 @@
 package sample.Model;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -18,15 +17,15 @@ public class Database {
     private static Connection getConnection() throws Exception {
         try {
             String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:8889/WildCats";
-            String username = "root";
-            String password = "wildcats123";
+            String url = "jdbc:mysql://academic-mysql.cc.gatech.edu/cs4400_Team_17";
+            String username = "cs4400_Team_17";
+            String password = "ngMBPElT";
             Class.forName(driver);
             Connection conn = DriverManager.getConnection(url, username, password);
             return conn;
         } catch (Exception e) {
+          System.out.println("Failed to connect");
             System.out.println(e);
-            System.out.println("Jatin probably doesn't have the server running right now");
         }
         return null;
     }
@@ -39,17 +38,26 @@ public class Database {
             Statement createTables = con.createStatement();
             ArrayList<String> createStatements = new ArrayList<>();
             //reading table statements from a txt file because I'm lazy...
-            Scanner scanner = new Scanner(new File("tables.txt"));
+            URL url = Database.class.getResource("tables.txt");
+            Scanner scanner = new Scanner(new File(url.getPath()));
             scanner.useDelimiter(";");
             //reads queries from file, separated by a ';'
             while(scanner.hasNext()) {
                 createStatements.add(scanner.next());
             }
             for (String table : createStatements) {
-                createTables.executeQuery(table);
+                System.out.println(table);
+                createTables.executeUpdate(table);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
+  public static void main(String[] args) {
+    try {
+      //setUp();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
