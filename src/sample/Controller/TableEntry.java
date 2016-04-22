c5205b7069340ad55dc80196d0ef2f27493f9f2a
Wong, Raymond <rwong1103@gatech.edu>
2016-04-21 21:51:16 -0400

    finished manager view

--- src/sample/Controller/TableEntry.java
+++ src/sample/Controller/TableEntry.java
@@ -1,5 +1,7 @@
 package sample.Controller;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 /**
@@ -10,6 +12,10 @@ public class TableEntry {
     public SimpleStringProperty time = new SimpleStringProperty();
     public SimpleStringProperty price1 = new SimpleStringProperty();
     public SimpleStringProperty price2 = new SimpleStringProperty();
+    public SimpleStringProperty month = new SimpleStringProperty();
+    public SimpleDoubleProperty revenue = new SimpleDoubleProperty();
+    public SimpleIntegerProperty numRes = new SimpleIntegerProperty();
+    public SimpleStringProperty train = new SimpleStringProperty();
 
     public String getName() {
         return name.get();
@@ -26,4 +32,12 @@ public class TableEntry {
     public String getPrice2() {
         return price2.get();
     }
+
+    public String getMonth() { return month.get(); }
+
+    public double getRevenue() {return revenue.get(); }
+
+    public int getNumRes() { return numRes.get(); }
+
+    public String getTrain() { return train.get(); }
 }
--- src/sample/Controller/ViewRevRep.java
+++ src/sample/Controller/ViewRevRep.java
@@ -1,17 +1,24 @@
 package sample.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import sample.Model.Database;
 
 import java.net.URL;
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.ResourceBundle;
+
 
 
 /**
@@ -21,9 +28,35 @@ public class ViewRevRep implements Initializable {
     @FXML
     Button back;
 
+    @FXML
+    TableView<TableEntry> table;
+
+    @FXML
+    TableColumn monthColumn;
+
+    @FXML
+    TableColumn revenueColumn;
+
+    ObservableList backing = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Calendar c = Calendar.getInstance();
+        table.setItems(backing);
+        monthColumn.setCellValueFactory(new PropertyValueFactory<TableEntry,
+                String>("month"));
+        revenueColumn.setCellValueFactory(new PropertyValueFactory<TableEntry,
+                Double>("revenue"));
+        String monthStr;
+        double rev;
+        for (int i =  3; i > 0; i--) {
+            monthStr = getMonth(c.get(Calendar.MONTH) - i);
+            rev = Database.getMonthRev(c.get(Calendar.MONTH) - (i - 1));
+            TableEntry t = new TableEntry();
+            t.month.set(monthStr);
+            t.revenue.set(rev);
+            backing.add(t);
+        }
         back.setOnAction(event -> {
             try {
                 Parent root = FXMLLoader.load(getClass().getResource
@@ -37,14 +70,14 @@ public class ViewRevRep implements Initializable {
                 e.printStackTrace();
             }
         });
-        // to build overservable list need to create observable list object
-        //ObservatbleList row =
-        String month1 = getMonth(c.get(Calendar.MONTH) - 3);
+
+        /*String month1 = getMonth(c.get(Calendar.MONTH) - 3);
         String month2 =  getMonth(c.get(Calendar.MONTH) - 2);
         String month3 =  getMonth(c.get(Calendar.MONTH) - 1);
-        System.out.println(month1);
-        System.out.println(month2);
-        System.out.println(month3);
+        double rev1 = Database.getMonthRev(c.get(Calendar.MONTH) - 2);
+        double rev2 = Database.getMonthRev(c.get(Calendar.MONTH) - 1);
+        double rev3 = Database.getMonthRev(c.get(Calendar.MONTH));*/
+
     }
     public String getMonth(int month) {
         return new DateFormatSymbols().getMonths()[month];
